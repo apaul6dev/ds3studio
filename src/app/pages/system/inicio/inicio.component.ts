93c8a9c0df31344c3965216bae16740fbc788a06
apaul6dev <apaul.gallegos@gmail.com>
2023-10-16 11:19:31 -0500

    agregacion de progress bar

--- src/app/pages/system/inicio/inicio.component.ts
+++ src/app/pages/system/inicio/inicio.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { InicioService } from './inicio.service';
 import { Settings } from "src/app/app.settings.model";
 import { AppSettings } from "src/app/app.settings";
 import { Router } from "@angular/router";
+import { MatProgressBar } from "@angular/material/progress-bar";
 
 @Component({
   selector: 'app-inicio',
@@ -11,6 +12,9 @@ import { Router } from "@angular/router";
 })
 export class InicioComponent implements OnInit {
 
+  progress: number = 0;
+  timerId: any;
+  duration: number = 3000;
 
   public settings: Settings;
   constructor(public appSettings: AppSettings, private inicioService: InicioService) {
@@ -19,6 +23,42 @@ export class InicioComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+
+
+
+
+
+
+
+
+
+  
+
+  startPressTimer() {
+    // Reiniciar el progreso
+    this.progress = 1;
+    this.timerId = setTimeout(() => {
+      console.log('Botón presionado por 3 segundos completos');
+    }, this.duration);
+    // Actualizar el progreso
+    this.updateProgress();
+  }
+
+  stopPressTimer() {
+    // Detener el temporizador si se suelta el botón antes de 3 segundos
+    clearTimeout(this.timerId);
+    // Reiniciar el progreso
+    this.progress = 0;
+  }
+
+  updateProgress() {
+    // Actualizar el progreso mientras el botón está presionado
+    if (this.progress > 0 && this.progress < 100) {
+      this.progress += 1;
+      setTimeout(() => this.updateProgress(), this.duration / 100);
+    }
   }
 
 
