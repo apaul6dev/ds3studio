edd18ecc87d6dc881c15bb1434d3fa925f0c690b
apaul6dev <apaul.gallegos@gmail.com>
2023-10-18 12:27:36 -0500

    corredcion de maps

--- src/app/pages/system/notificaciones/detalle-notificacion/detalle-notificacion.component.ts
+++ src/app/pages/system/notificaciones/detalle-notificacion/detalle-notificacion.component.ts
@@ -3,7 +3,6 @@ import { DetalleNotificacionService } from './detalle-notificacion.service';
 import { Router } from "@angular/router";
 import { Settings } from "src/app/app.settings.model";
 import { AppSettings } from "src/app/app.settings";
-import { MapDirectionsService } from '@angular/google-maps';
 
 @Component({
     selector: 'app-detalle-notificacion',
@@ -13,54 +12,33 @@ import { MapDirectionsService } from '@angular/google-maps';
 export class DetalleNotificacionComponent implements OnInit {
     notification: any;
 
-    settings: Settings; 
+    settings: Settings;
     center: google.maps.LatLngLiteral = { lat: 45.421530, lng: -75.697193 };
-    zoom = 7;
+    zoom = 12;
     markerOptions: google.maps.MarkerOptions = { draggable: false };
     markerPositions: google.maps.LatLngLiteral[] = [
-      { lat: 45.421530, lng: -75.697193 }
+        { lat: 45.421530, lng: -75.697193 }
     ];
 
-    constructor(private mapDirectionsService: MapDirectionsService,
+    constructor(
         private router: Router, public appSettings: AppSettings, private detalleNotificacionService: DetalleNotificacionService) {
         this.settings = this.appSettings.settings;
 
         this.notification = this.detalleNotificacionService.notification;
+        
         console.log("recibiendo notificacion: ", this.notification);
 
         this.center.lat = this.notification.latitud;
         this.center.lng = this.notification.longitud;
 
-    }
+        this.markerPositions[0].lat = this.notification.latitud;
+        this.markerPositions[0].lng = this.notification.longitud;
 
+    }
 
 
     ngOnInit() {
-       //  this.calcularRuta();
-    }
 
-
-    calcularRuta() {
-        // Define the origin and destination points
-        const origin = { lat: 40.7128, lng: -74.0060 }; // Example: New York
-        const destination = { lat: 34.0522, lng: -118.2437 }; // Example: Los Angeles
-
-        // Configure the route options
-        const routeOptions = {
-            origin: origin,
-            destination: destination,
-            travelMode: google.maps.TravelMode.DRIVING, // You can use 'WALKING', 'BICYCLING', etc.
-        };
-
-        // Call MapDirectionsService to calculate the route
-        this.mapDirectionsService.route(routeOptions)
-            .subscribe((result) => {
-                // 'result' contains information about the route
-                console.log(result);
-            }, (error) => {
-                // Error handling
-                console.error('Error calculating the route:', error);
-            });
     }
 
 
