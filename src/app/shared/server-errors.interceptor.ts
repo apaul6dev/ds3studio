e375de6fbfeb4b8258d1a3fc7cb277a56c5c7253
apaul6dev <apaul.gallegos@gmail.com>
2023-10-19 13:20:48 -0500

    correcion de mensajes del servidor

--- src/app/shared/server-errors.interceptor.ts
+++ src/app/shared/server-errors.interceptor.ts
@@ -21,24 +21,22 @@ export class ServerErrorsInterceptor implements HttpInterceptor {
                     if (event.body && event.body.error === true && event.body.errorMessage) {
                         throw new Error(event.body.errorMessage);
                     }/*else{
-                        this.snackBar.open("EXITO", 'AVISO', { duration: 5000 });    
-                    }*/
+                        this.snackBar.open("BIENVENIDO", 'AVISO', { duration: 5000 });    
+                    } */
                 }
             })).pipe(catchError((err) => {
                 console.log(err);
                 //https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
                 if (err.status === 400) {
-                    this.snackBar.open(err.mensaje, 'ERROR 400', { duration: 5000 });
+                    this.snackBar.open(err.message, 'ERROR 400', { duration: 5000 });
                 }
                 else if (err.status === 401) {
-                    console.log(err.message);
                     this.snackBar.open(err.message, 'ERROR 401', { duration: 5000 });
-                    //this.router.navigate(['/login']);
                 }
                 else if (err.status === 500) {
-                    this.snackBar.open(err.error.mensaje, 'ERROR 500', { duration: 5000 });
+                    this.snackBar.open(err.message, 'ERROR 500', { duration: 5000 });
                 } else {
-                    this.snackBar.open(err.error.mensaje, 'ERROR', { duration: 5000 });
+                    this.snackBar.open(err.message, 'ERROR', { duration: 5000 });
                 }
                 return EMPTY;
             }));
