f776c47d215d6d498b6281b7f343e8f6dec539e0
apaul6dev <apaul.gallegos@gmail.com>
2023-11-12 22:10:10 -0500

    add comodin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://modosmart.d3studio.tk/dark/rest',
-    dominio: 'https://modosmart.d3studio.tk/dark/',
+    firebase: {
+        apiKey: "AIzaSyBB5xCP7i0x4NOm1bA2Sk-qpQjeMl57sAc",
+        authDomain: "modosmart-2506c.firebaseapp.com",
+        projectId: "modosmart-2506c",
+        storageBucket: "modosmart-2506c.appspot.com",
+        messagingSenderId: "32762457249",
+        appId: "1:32762457249:web:adbbd4d85e7c79be50d4e0",
+        measurementId: "G-EGP4W39SEZ"
+      },
+      production: true,
+      apiUrl: 'https://modosmart.d3studio.tk/dark/rest',
+      dominio: 'https://modosmart.d3studio.tk/dark/',
 };
